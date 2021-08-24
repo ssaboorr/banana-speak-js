979fe348dee0c3580e0fa489de6803706af8baee
ssaboorr <sabooransari777@gmail.com>
2021-08-24 15:14:10 +0530

    Taken Input

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-// console.log("js is working")
+// console.log("js is working")#
 
 // var userName = prompt("whats Your username?")
 
 // alert("welcome " + userName)
+
+var btnTranslate = document.querySelector("#translate-btn")
+var textInput = document.querySelector("#text-input")
+
+// console.log(textInput)
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("Clicked")
+    console.log("input: " + textInput.value);
+})
